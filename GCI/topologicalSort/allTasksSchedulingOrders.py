ab2ea2a17311ca0fd7e16278e61b25491d231fff
Albert-Weon-Jun-Oh <Albert.Weon.Jun.Oh@ibm.com>
2021-01-01 00:13:37 -0400

    get all tasks scheduling orders

--- GCI/topologicalSort/allTasksSchedulingOrders.py
+++ GCI/topologicalSort/allTasksSchedulingOrders.py
@@ -9,4 +9,53 @@ def getAllTaskScheduleOrders(tasks, prereqs):
     if tasks <= 0:
         return []
 
-def generateAllSchedules(queue, )
+    in_degree = defaultdict(lambda: 0)
+    graph = defaultdict(lambda: set())
+
+    for source, target in prereqs:
+        graph[source].add(target)
+        in_degree[target] += 1
+
+    queue = []
+    for node in range(tasks):
+        if in_degree[node] == 0:
+            queue.append(node)
+
+    print('tasks -> ', tasks)
+    print('start queue -> ', queue)
+    output = []
+    generateAllSchedules(tasks, graph, in_degree, queue, [], output)
+    return output
+
+def generateAllSchedules(tasks, graph, in_degree, queue, current, output):
+    if queue:
+        for vertex in queue:
+            current.append(vertex)
+            next_queue = list(filter(lambda x: x != vertex, queue))
+
+            for child in graph[vertex]:
+                in_degree[child] -= 1
+                if in_degree[child] == 0:
+                    next_queue.append(child)
+
+            generateAllSchedules(tasks, graph, in_degree, next_queue, current, output)
+
+            current.pop()
+            for child in graph[vertex]:
+                in_degree[child] += 1
+
+    if len(current) == tasks:
+        output.append(current[:])
+
+if __name__ == "__main__":
+    vertices = 5
+    edges = [[4, 2],
+            [4, 3],
+            [2, 0],
+            [2, 1],
+            [3, 1]]
+    print(getAllTaskScheduleOrders(vertices, edges))
+
+    vertices = 7
+    edges = [[6, 4], [6, 2], [5, 3], [5, 4], [3, 0], [3, 1], [3, 2], [4, 1]]
+    print(getAllTaskScheduleOrders(vertices, edges))
