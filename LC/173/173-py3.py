67eb85829c1cad30fe0ad162e5b4011cd2de395c
Weon Jun (Albert) Oh <albertopha@hotmail.com>
2022-05-04 21:46:08 -0400

    [FB] 173-py3.py
    https://leetcode.com/problems/binary-search-tree-iterator/

---
+++ LC/173/173-py3.py
@@ -0,0 +1,36 @@
+"""
+stack = [15,9]
+node = 
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self.getNextNodes(root)
+    
+    def getNextNodes(self, node: Optional[TreeNode]) -> None:
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        node = self.stack.pop()
+        if node.right:
+            self.getNextNodes(node.right)
+        return node.val
+
+    def hasNext(self) -> bool:
+        return self.stack
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
