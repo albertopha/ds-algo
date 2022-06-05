718b024fa4897c3bcc50bf0c1800c0922aed72e2
Weon Jun (Albert) Oh <albertopha@hotmail.com>
2022-06-05 18:50:00 -0400

    [FB] (PS | TLE) 212-py3.py
    https://leetcode.com/problems/word-search-ii/

---
+++ LC/212/212-py3.py
@@ -0,0 +1,68 @@
+"""
+TLE for:
+[["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"]]
+["lllllll","fffffff","ssss","s","rr","xxxx","ttt","eee","ppppppp","iiiiiiiii","xxxxxxxxxx","pppppp","xxxxxx","yy","jj","ccc","zzz","ffffffff","r","mmmmmmmmm","tttttttt","mm","ttttt","qqqqqqqqqq","z","aaaaaaaa","nnnnnnnnn","v","g","ddddddd","eeeeeeeee","aaaaaaa","ee","n","kkkkkkkkk","ff","qq","vvvvv","kkkk","e","nnn","ooo","kkkkk","o","ooooooo","jjj","lll","ssssssss","mmmm","qqqqq","gggggg","rrrrrrrrrr","iiii","bbbbbbbbb","aaaaaa","hhhh","qqq","zzzzzzzzz","xxxxxxxxx","ww","iiiiiii","pp","vvvvvvvvvv","eeeee","nnnnnnn","nnnnnn","nn","nnnnnnnn","wwwwwwww","vvvvvvvv","fffffffff","aaa","p","ddd","ppppppppp","fffff","aaaaaaaaa","oooooooo","jjjj","xxx","zz","hhhhh","uuuuu","f","ddddddddd","zzzzzz","cccccc","kkkkkk","bbbbbbbb","hhhhhhhhhh","uuuuuuu","cccccccccc","jjjjj","gg","ppp","ccccccccc","rrrrrr","c","cccccccc","yyyyy","uuuu","jjjjjjjj","bb","hhh","l","u","yyyyyy","vvv","mmm","ffffff","eeeeeee","qqqqqqq","zzzzzzzzzz","ggg","zzzzzzz","dddddddddd","jjjjjjj","bbbbb","ttttttt","dddddddd","wwwwwww","vvvvvv","iii","ttttttttt","ggggggg","xx","oooooo","cc","rrrr","qqqq","sssssss","oooo","lllllllll","ii","tttttttttt","uuuuuu","kkkkkkkk","wwwwwwwwww","pppppppppp","uuuuuuuu","yyyyyyy","cccc","ggggg","ddddd","llllllllll","tttt","pppppppp","rrrrrrr","nnnn","x","yyy","iiiiiiiiii","iiiiii","llll","nnnnnnnnnn","aaaaaaaaaa","eeeeeeeeee","m","uuu","rrrrrrrr","h","b","vvvvvvv","ll","vv","mmmmmmm","zzzzz","uu","ccccccc","xxxxxxx","ss","eeeeeeee","llllllll","eeee","y","ppppp","qqqqqq","mmmmmm","gggg","yyyyyyyyy","jjjjjj","rrrrr","a","bbbb","ssssss","sss","ooooo","ffffffffff","kkk","xxxxxxxx","wwwwwwwww","w","iiiiiiii","ffff","dddddd","bbbbbb","uuuuuuuuu","kkkkkkk","gggggggggg","qqqqqqqq","vvvvvvvvv","bbbbbbbbbb","nnnnn","tt","wwww","iiiii","hhhhhhh","zzzzzzzz","ssssssssss","j","fff","bbbbbbb","aaaa","mmmmmmmmmm","jjjjjjjjjj","sssss","yyyyyyyy","hh","q","rrrrrrrrr","mmmmmmmm","wwwww","www","rrr","lllll","uuuuuuuuuu","oo","jjjjjjjjj","dddd","pppp","hhhhhhhhh","kk","gggggggg","xxxxx","vvvv","d","qqqqqqqqq","dd","ggggggggg","t","yyyy","bbb","yyyyyyyyyy","tttttt","ccccc","aa","eeeeee","llllll","kkkkkkkkkk","sssssssss","i","hhhhhh","oooooooooo","wwwwww","ooooooooo","zzzz","k","hhhhhhhh","aaaaa","mmmmm"]
+"""
+from collections import deque
+
+class Node:
+    
+    def __init__(self, char):
+        self.char = char
+        self.is_word = False
+        self.next = dict()
+
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        if not board or not board[0] or not words:
+            return []
+        
+        m, n = len(board), len(board[0])
+        output = set()
+        trie = self.buildTrie(words)
+        
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] in trie.next:
+                    self.bfs(board, row, col, trie, output)
+        return list(output)
+    
+    def bfs(self, board: List[List[str]], row: int, col: int, trie: Optional[Node], output: set) -> None:
+        m, n = len(board), len(board[0])
+        dirs = [[-1,0],[0,1],[1,0],[0,-1]]
+        cchar = board[row][col]
+        queue = deque([(row, col, trie.next[cchar], cchar, set([(row, col)]))])
+        
+        while queue:
+            r, c, node, word, visited = queue.popleft()
+            
+            if node.is_word:
+                output.add(word)
+            
+            for drow, dcol in dirs:
+                nrow, ncol = r+drow, c+dcol
+                
+                if nrow < 0 or nrow >= m or\
+                    ncol < 0 or ncol >= n or\
+                    (nrow, ncol) in visited or\
+                    board[nrow][ncol] not in node.next:
+                    continue
+                
+                nchar = board[nrow][ncol]
+                new_visited = visited.copy()
+                new_visited.add((nrow, ncol))
+                queue.append((nrow, ncol, node.next[nchar], word+nchar, new_visited))
+    
+    def buildTrie(self, words: List[str]) -> Optional[Node]:
+        trie = Node('')
+        
+        for word in words:
+            node = trie
+            for char in word:
+                if char not in node.next:
+                    node.next[char] = Node(char)
+                node = node.next[char]
+            node.is_word = True
+        return trie
+    
